ea3a453bf0533c2295eef205af0175c45d53acb8
Vasilij U <vasya-1001@yandex.ru>
2023-04-24 19:50:58 +0500

    Zadacha 25 completed

--- Zadacha_25/Program.cs
+++ Zadacha_25/Program.cs
@@ -1,7 +1,34 @@
 ﻿Console.Clear();
 
-void
+void start()
+{
+    bool work = true;
+    while (work)
+    {
+        System.Console.WriteLine("Решаем задачу? (y/n)");
+        string i= System.Console.ReadLine();
+        switch (i)
+        {
+            case "y": task();break;
+            case "n": work = false; break;
+            default: System.Console.WriteLine("Something goes wrong. Try again."); break;
+        }
+    }
+}
 
+start();
 
-System.Console.WriteLine("Enter number from 10000 to 99999: ");
-int a = int.Parse(System.Console.ReadLine());
+void task()
+{
+    System.Console.WriteLine("Введите число А: ");
+    int num1 = int.Parse(System.Console.ReadLine());
+    System.Console.WriteLine("Введите число Б: ");
+    int num2 = int.Parse(System.Console.ReadLine());
+    System.Console.WriteLine($"Число А в степени Б = {MathPath(num1, num2)}");
+}
+
+double MathPath(int a, int b)
+{
+    double result = Math.Pow(a,b);
+    return result;
+}

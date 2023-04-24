89e69c8bf2d6a6b245359b3f813fe956e0d598f4
Vasilij U <vasya-1001@yandex.ru>
2023-04-24 20:56:03 +0500

    Zadacha 29 completed

--- Zadacha_29/Program.cs
+++ Zadacha_29/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+void start()
+{
+    bool work = true;
+    while (work)
+    {
+        System.Console.WriteLine("Решаем задачу? (y/n)");
+        string i= System.Console.ReadLine();
+        switch (i)
+        {
+            case "y": task();break;
+            case "n": work = false; break;
+            default: System.Console.WriteLine("Something goes wrong. Try again."); break;
+        }
+    }
+}
+
+start();
+
+void task()
+{
+int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array [i] = new Random().Next(0,999);
+    }
+System.Console.WriteLine(string.Join(", ", array));
+}
